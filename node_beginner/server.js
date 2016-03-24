ad0494e357fa65c2d5a4951b063f8ab561865a9e
ssergejev <sergei.sergejev@myjar.com>
2016-03-24 12:56:55 +0200

    assign function to var

--- node_beginner/server.js
+++ node_beginner/server.js
@@ -1,9 +1,19 @@
 var http = require("http");
 var url = require("url");
 
-// anonymous function is used by default as first and only parameter of createServer()
-http.createServer(function(request, response) {
+// // anonymous function is used by default as first and only parameter of createServer()
+// http.createServer(function(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write("Hello World");
+//     response.end();
+// }).listen(8888);
+//
+// // it can be assigned to var and var can be passed to createServer()
+var onRequest = function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
-}).listen(8888);
+}; // NB! must terminate assignment operation, yay @ IDEA
+http.createServer(onRequest).listen(8888);
+
+console.log("Server has started");
