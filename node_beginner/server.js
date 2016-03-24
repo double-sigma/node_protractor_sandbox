5d9919f662b2073804f4e1d7a84605dccb2661fb
ssergejev <sergei.sergejev@myjar.com>
2016-03-24 12:57:50 +0200

    simply name a function

--- node_beginner/server.js
+++ node_beginner/server.js
@@ -9,11 +9,22 @@ var url = require("url");
 // }).listen(8888);
 //
 // // it can be assigned to var and var can be passed to createServer()
-var onRequest = function(request, response) {
+// var onRequest = function(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write("Hello World");
+//     response.end();
+// }; // NB! must terminate assignment operation, yay @ IDEA
+// http.createServer(onRequest).listen(8888);
+
+
+// or we can simply name this function and pass its name
+function onRequest(request, response) { // request and response are returned on each callback of createServer
+    console.log("Request for received");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
-}; // NB! must terminate assignment operation, yay @ IDEA
-http.createServer(onRequest).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888); // on request function receives request, response from createServer
 
 console.log("Server has started");
