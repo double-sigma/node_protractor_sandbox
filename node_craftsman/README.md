8810c84bcbff3d235e80014090b983901660b609
sserzant <sergei.sergejev@myjar.com>
2016-03-28 12:39:51 +0300

    new project for node craftsman

---
+++ node_craftsman/README.md
@@ -0,0 +1,13 @@
+# Sandbox
+
+Sandbox to play with The Node Craftsman Book by Manuel Kiessling
+
+# The project
+* Test-driven by Jasmine
+
+# Running
+* to run Jasmine tests execute `./node_modules/jasmine-node/bin/jasmine-node spec/greetSpec.js`
+
+
+# To read
+* https://npmjs.org/doc/json.html#dependencies
---
+++ node_craftsman/spec/greetSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+var greet = require('../src/greet');
+
+describe('greet', function() { // suite
+
+    it('should greet the given name if name is given', function() { // single test
+        expect(greet('Joe')).toEqual('Hello Joe!');
+    });
+
+    it('should greet world if no name is given', function() {
+        expect(greet()).toEqual('Hello world!');
+    });
+});
