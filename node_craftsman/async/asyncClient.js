c3587b8bfbb98f5f535a28edc6f0d9bcd38a1109
sserzant <sergei.sergejev@myjar.com>
2016-03-29 14:47:48 +0300

    another try to run code faster, but it's wrong

--- node_craftsman/async/asyncClient.js
+++ node_craftsman/async/asyncClient.js
@@ -1,16 +1,18 @@
 'use strict';
 
 var request = require('request');
+var name, status;
 
 request.get('http://localhost:8080/getUserName?id=1234', function (err, res, body) {
     var result = JSON.parse(body);
-    var name = result.value;
-
-    // we start this only after first request finishes
-    request.get('http://localhost:8080/getUserStatus?id=1234', function (err, res, body) {
-        var result = JSON.parse(body);
-        var status = result.value;
-        console.log('The status of user', name, 'is', status);
-    });
+    name = result.value;
 });
 
+request.get('http://localhost:8080/getUserStatus?id=1234', function (err, res, body) {
+    var result = JSON.parse(body);
+    status = result.value;
+});
+
+// this will result in The status of user undefined is undefined because
+// console.log will run in same even-loop as requests
+console.log('The status of user', name, 'is', status);
