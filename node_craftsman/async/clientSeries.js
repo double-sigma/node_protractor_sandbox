79a1fba092bc941b9311062e565d56b559eea1d4
sserzant <sergei.sergejev@myjar.com>
2016-03-29 15:21:49 +0300

    asnyc-series rquest example

--- node_craftsman/async/clientSeries.js
+++ node_craftsman/async/clientSeries.js
@@ -1,28 +1,43 @@
 'use strict';
 
 var request = require('request');
+var async = require('async');
 
 var url = 'http://localhost:8080/';
 
+// same result as boomerang, but much more readable code
+// series runs request one after another in guaranteed order
+async.series([
+    function (callback) {
+        request.get(url + 'getUserName?id=1234',
+            function (err, res, body) {
+                console.log('Name:', JSON.parse(body).value);
+                callback(null);
+            });
+    },
 
-// boomerang pattern in acttion
-request.get(url + 'getUserName?id=1234', function (err, res, body) {
+    function (callback) {
+        request.get(url + 'getUserStatus?id=1234',
+            function (err, res, body) {
+                console.log('Status:', JSON.parse(body).value);
+                callback(null);
+            });
+    },
 
-    console.log('Name:', JSON.parse(body).value);
+    function (callback) {
+        request.get(url + 'getUserCountry?id=1234',
+            function (err, res, body) {
+                console.log('Country:', JSON.parse(body).value);
+                callback(null);
+            });
+    },
 
-    request.get(url + 'getUserStatus?id=1234',
-        function (err, res, body) {
-            console.log('Status:', JSON.parse(body).value);
-
-            request.get(url + 'getUserCountry?id=1234',
-                function (err, res, body) {
-                    console.log('Country:', JSON.parse(body).value);
-
-                    request.get(url + 'getUserAge?id=1234',
-                        function (err, res, body) {
-                            console.log('Age:', JSON.parse(body).value);
-                        });
-                });
-        });
-});
-
+    function (callback) {
+        request.get(url + 'getUserAge?id=1234',
+            function (err, res, body) {
+                console.log('Age:', JSON.parse(body).value);
+                callback(null);
+            });
+    }
+    
+]);
