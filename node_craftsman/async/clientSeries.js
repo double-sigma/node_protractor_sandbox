a4311b5ededb2a30015af7f0ff6c86e6c671f660
sserzant <sergei.sergejev@myjar.com>
2016-03-30 11:59:13 +0300

    added error handling to series requests. Otherwise it crashed with unexpected array value

--- node_craftsman/async/clientSeries.js
+++ node_craftsman/async/clientSeries.js
@@ -7,33 +7,54 @@ var url = 'http://localhost:8080/';
 
 // same result as boomerang, but much more readable code
 // series runs request one after another in guaranteed order
+// In case that one of the series steps passes a non-null value to its callback as the first parameter,
+// the series is immediately stops, and the final callback is triggered with the results that
+// have been collected to far, and the err parameter set to the error value passed by the failing step
 async.series([
 
         function (callback) {
             request.get(url + 'getUserName?id=1234',
                 function (err, res, body) {
-                    callback(null, 'Name: ' + JSON.parse(body).value);
+                    // callback is called once request finishes
+                    if (err) {
+                        callback(err);
+                    } else {
+                        callback(err, 'Name: ' + JSON.parse(body).value);
+                    }
                 });
         },
 
         function (callback) {
             request.get(url + 'getUserStatus?id=1234',
                 function (err, res, body) {
-                    callback(null, 'Status: ' + JSON.parse(body).value);
+                    if (err) {
+                        callback(err);
+                    } else {
+                        callback(err, 'Status: ' + JSON.parse(body).value);
+                    }
                 });
         },
 
         function (callback) {
             request.get(url + 'getUserCountry?id=1234',
                 function (err, res, body) {
-                    callback(null, 'Country: ' + JSON.parse(body).value);
+                    if (err) {
+                        callback(err);
+                    } else {
+                        callback(err, 'Country: ' + JSON.parse(body).value);
+                    }
                 });
         },
 
         function (callback) {
-            request.get(url + 'getUserAge?id=1234',
+            request.get('http://localhost:9999/none',
+                //request.get(url + 'getUserAge?id=1234',
                 function (err, res, body) {
-                    callback(null, 'Age: ' + JSON.parse(body).value);
+                    if (err) {
+                        callback(err);
+                    } else {
+                        callback(err, 'Age: ' + JSON.parse(body).value);
+                    }
                 });
         }
 
