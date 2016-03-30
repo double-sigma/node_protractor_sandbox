d7f53c19bc0050a20e9badea261e9637982b91ab
sserzant <sergei.sergejev@myjar.com>
2016-03-30 18:12:02 +0300

    quering db with stream

--- node_craftsman/mysql/index.js
+++ node_craftsman/mysql/index.js
@@ -11,26 +11,33 @@ var connection = mysql.createConnection({
 });
 
 // TODO make it synchronous and check with eg http request and sleep(10)
-connection.query(
-    // large resultsets is another source of potentially blocking behaviour
-    // Using the callback pattern ensures that our Node.js application won’t block while DB retrieves 10000000 rows
-    // but when node starts operating on them, eg logging - it will block
-    'SELECT id, content FROM test;',
-    // This function gets called when Node.js receives the answer to our query from the server - a callback.
-    function (err, results, fields) {
-        // results is simply an array of objects, with each object representing one row of the result set
-        // and each attribute of such an object representing one field of the according row
-        // [ { first_field: 'foo', second_field: 'bar' } ]
-        // [ array ]
-        // [ {object} ]
-        // [ { objects_attribute: 'attribute value' } ]
-        if (!err) {
-            console.log(results);
-        } else {
-            console.log(err);
-        }
+// large resultsets is another source of potentially blocking behaviour
+// Using the callback pattern ensures that our Node.js application won’t block while DB retrieves 10000000 rows
+// but when node starts operating on them, eg logging - it will block
 
+// to avoid grabbing whole query result at once, we should use streaming API (same as file reader, with events)
+var query = connection.query('SELECT id, content FROM test');
+query.on('error',
+    function (err) {
+        console.log('A database error occured:');
+        console.log(err);
+    });
+
+query.on('fields',
+    function (fields) {
+        console.log('Received fields information.');
+    });
+
+query.on('result',
+    function (result) {
+        console.log('Received result:');
+        console.log(result);
+    });
+
+query.on('end',
+    function () {
+        console.log('Query execution has finished.');
         connection.end();
-    }
-);
+    });
 
+// NB the order of the on blocks doesn’t say anything about when (and if) they are executed
