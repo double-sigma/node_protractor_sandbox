8b6dec2d5f72ff6699b348d8b9ada44980bd1d70
sserzant <sergei.sergejev@myjar.com>
2016-03-31 13:30:04 +0300

    collection insert -> collection update

--- node_craftsman/mongodb-test/index.js
+++ node_craftsman/mongodb-test/index.js
@@ -7,9 +7,13 @@ MongoClient.connect('mongodb://127.0.0.1:27017/accounting',
     function (err, connection) {
 
         var collection = connection.collection('customers');
-        collection.insert({'name': 'Jane Doe'},
 
+        // MongoDB updates only the first match for a given filter
+        // even if it matches every document
+        // multir true is needed to update all matching documents
+        collection.update({}, {'$set': {'age': 24}}, {'multi': true},
             function (err, count) {
+                console.log('Updated', count, 'documents');
                 collection.find().toArray(
                     function (err, documents) {
                         console.dir(documents);
