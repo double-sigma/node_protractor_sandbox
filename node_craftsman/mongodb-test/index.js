02a1379350f38f174503565445b0e1e27ee62cfa
sserzant <sergei.sergejev@myjar.com>
2016-03-31 15:26:54 +0300

    insert, update & delete mongo exmaple

--- node_craftsman/mongodb-test/index.js
+++ node_craftsman/mongodb-test/index.js
@@ -5,19 +5,56 @@ var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://127.0.0.1:27017/accounting',
 
     function (err, connection) {
+        var collection = connection.collection('customers2');
 
-        var collection = connection.collection('customers');
-
-        // MongoDB updates only the first match for a given filter
-        // even if it matches every document
-        // multir true is needed to update all matching documents
-        collection.update({}, {'$set': {'age': 24}}, {'multi': true},
-            function (err, count) {
-                console.log('Updated', count, 'documents');
-                collection.find().toArray(
-                    function (err, documents) {
-                        console.dir(documents);
-                        connection.close();
-                    });
+        var listDocuments = function (callback) {
+            collection.find().toArray(function (err, documents) {
+                console.dir(documents);
+                callback();
             });
+        };
+
+        var doInsert = function (i) {
+            if (i < 20) {
+                var value = Math.floor(Math.random() * 10);
+                collection.insert(
+                    {'n': '#' + i, 'v': value}, function (err, count) {
+                        doInsert(i + 1);
+                    });
+            } else {
+                console.log();
+                console.log('Inserted', i, 'documents:');
+                listDocuments(function () {
+                    doUpdateAndDelete();
+                });
+            }
+        };
+
+
+        var doUpdateAndDelete = function () {
+            collection.update(
+                {'v': {'$gt': 5}}, // all whose "v > 5"
+                {'$set': {'valuable': true}},
+                {'multi': true},
+                function (err, count) {
+                    console.log();
+                    console.log('Updated', count, 'documents:');
+                    listDocuments(function () {
+                        // remove doesn’t need a multi parameter in order to work on all matched documents
+                        // it does so by default
+                        //collection.remove({}, function () {
+                            connection.close();
+                        //});
+                    });
+
+                });
+        };
+
+        // insert 20 records
+        // update those whose v > 5
+        // list documents
+        // delete all documents
+        doInsert(0);
+
+
     });
