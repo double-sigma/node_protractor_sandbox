e588cf63ca0f65126a681fd98dd8dfe620e1e81c
sserzant <sergei.sergejev@myjar.com>
2016-04-01 16:14:59 +0300

    added first GET route. Test for this route. Added sqlite

--- node_craftsman/angular_app/spec/e2e/apiSpec.js
+++ node_craftsman/angular_app/spec/e2e/apiSpec.js
@@ -1,19 +1,69 @@
 'use strict';
 
 var request = require('request');
+var dbSession = require('../../src/backend/dbSession.js');
+var resetDatabase = require('../resetDb.js');
+var async = require('async');
 
 describe('The API', function () {
 
     it('should response to a GET request at /api/keywords/', function (done) {
-        request.get(
-            {
-                'url': 'http://localhost:8080/api/keywords/',
-                'json': true
-            },
-            function (err, res, body) {
-                expect(res.statusCode).toBe(200);
-                expect(body.foo).toEqual('bar');
-                done();
+
+        var expected = {
+            // _items contains the actual list of keywords. Percolator doesn't allow to return an array of things directly.
+            "_items": [
+                {'id': 1, 'value': 'Aubergine', 'categoryID': 1},
+                {'id': 2, 'value': 'Onion', 'categoryID': 1},
+                {'id': 3, 'value': 'Knife', 'categoryID': 2}
+            ]
+        };
+
+
+        async.series(
+            [
+                function (callback) {
+                    resetDatabase(dbSession, callback);
+                },
+
+                function (callback) {
+                    dbSession.insert(
+                        'keyword',
+                        {'value': 'Aubergine', 'categoryID': 1},
+                        function (err) {
+                            callback(err);
+                        });
+                },
+
+                function (callback) {
+                    dbSession.insert(
+                        'keyword',
+                        {'value': 'Onion', 'categoryID': 1},
+                        function (err) {
+                            callback(err);
+                        });
+                },
+
+                function (callback) {
+                    dbSession.insert(
+                        'keyword',
+                        {'value': 'Knife', 'categoryID': 2},
+                        function (err) {
+                            callback(err);
+                        });
+                }
+            ],
+
+            function (err, results) {
+                request.get(
+                    {
+                        'url': 'http://localhost:8585/api/keywords/',
+                        'json': true
+                    },
+                    function (err, res, body) {
+                        expect(res.statusCode).toBe(200);
+                        expect(body).toEqual(expected);
+                        done();
+                    });
             });
     });
 
--- node_craftsman/angular_app/src/backend/index.js
+++ node_craftsman/angular_app/src/backend/index.js
@@ -1,15 +1,38 @@
 'use strict';
 
 var Percolator = require('Percolator').Percolator;
+var dbSession = require('../../src/backend/dbSession.js');
 
-var port = 8080;
-var server = Percolator({'port': port});
+var port = 8585;
+var server = Percolator({'port': port, 'autoLink': false});
 
-server.route('/api/keywords', {
-    GET: function (req, res) {
-        res.object({'foo': 'bar'}).send();
+server.route('/',
+    {
+
+        GET: function (req, res) {
+            console.log('routing...');
+            res.object({'foo': 'bar'}).send();
+        }
     }
-});
+);
+
+server.route('/api/keywords',
+    {
+
+        GET: function (req, res) {
+            console.log('routing...');
+            dbSession.fetchAll('SELECT id, value, categoryID FROM keyword ORDER BY id', function (err, rows) {
+                if (err) {
+                    console.log(err);
+                    res.status.internalServerError(err);
+                } else {
+                    console.log('success');
+                    res.collection(rows).send();
+                }
+            });
+        }
+    }
+);
 
 server.listen(function () {
     console.log('Server started on port ', port);
